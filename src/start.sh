955be926f290f9ab9bed7fba951096f872457386
Kielas <c1470759@outlook.com>
2025-07-25 00:24:27 +0800

    adwa

--- src/start.sh
+++ src/start.sh
@@ -2,4 +2,5 @@
 echo "520" | sudo -S chmod 777 /home/kielas/2025_elc/src/start.sh
 echo "520" | sudo -S chmod 777 /home/kielas/2025_elc/src/main.py
 v4l2-ctl --list-devices  # 列出所有视频设备
+# sudo rdk-miniboot-update
 /usr/bin/python3 /home/kielas/2025_elc/src/main.py

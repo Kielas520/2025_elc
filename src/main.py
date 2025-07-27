10a8cbfc6531213e37c599734749ee0027fcc3cc
kielas520 <c1470759@outlook.com>
2025-07-27 11:11:26 +0800

    gssgseg

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ def init_board():
     #cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
     #cv2.resizeWindow('Camera', 640, 480)
     cv2.namedWindow('Mask', cv2.WINDOW_NORMAL)
-    #cv2.namedWindow('board', cv2.WINDOW_NORMAL)
     #cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
     # Create trackbars for HSV thresholds (initial values for light yellow)
     cv2.namedWindow('Controls')
@@ -24,10 +23,6 @@ def init_board():
     cv2.createTrackbar('V Min', 'Controls', 42, 255, nothing) # Value min
     cv2.createTrackbar('V Max', 'Controls', 101, 255, nothing) # Value max
     cv2.createTrackbar('light_area', 'Controls', 412, 20000, nothing)
-    cv2.createTrackbar('board_min_area', 'Controls', 81000, 200000, nothing)
-    cv2.createTrackbar('bin_thresh', 'Controls', 50, 255, nothing)
-    cv2.createTrackbar('canny_min', 'Controls', 50, 255, nothing)
-    cv2.createTrackbar('canny_max', 'Controls', 150, 255, nothing)
     cv2.createTrackbar('kernel_x', 'Controls', 3, 10, nothing)
     cv2.createTrackbar('kernel_y', 'Controls', 3, 10, nothing)
 
@@ -40,20 +35,12 @@ def update_hsv():
     v_min = cv2.getTrackbarPos('V Min', 'Controls')
     v_max = cv2.getTrackbarPos('V Max', 'Controls')
     light_area = cv2.getTrackbarPos('light_area', 'Controls')
-    board_min_area = cv2.getTrackbarPos('board_min_area', 'Controls')
-    bin_thresh = cv2.getTrackbarPos('bin_thresh', 'Controls')
-    canny_min = cv2.getTrackbarPos('canny_min', 'Controls')
-    canny_max = cv2.getTrackbarPos('canny_max', 'Controls')
     kernel_x = cv2.getTrackbarPos('kernel_x', 'Controls')
     kernel_y = cv2.getTrackbarPos('kernel_y', 'Controls')
     # Create HSV threshold range
     detector.bgr_lower = (h_min, s_min, v_min)
     detector.bgr_upper = (h_max, s_max, v_max)
     detector.light_min_area = light_area
-    detector.board_min_area = board_min_area
-    detector.bin_val = bin_thresh
-    detector.canny_min = canny_min
-    detector.canny_max = canny_max
     detector.kernel_x = kernel_x
     detector.kernel_y = kernel_y
 
@@ -80,7 +67,6 @@ def main():
         serial.send_data(-yaw * 0.6, -pitch * 0.6)
         #cv2.imshow('Camera', frame)
         cv2.imshow('Mask', detector.mask)
-        #cv2.imshow('board', detector.binary)
         #cv2.imshow('Result', result)
 
         # 计算 FPS
@@ -98,7 +84,7 @@ def main():
     cv2.destroyAllWindows()
 
 cam = camera.Camera(index=0, format='MJPG', width=640, height=480, fps=120)
-detector = Detector.Detector(color = [(13, 255, 152), (0, 51, 110)], light_min_area=5, board_min_area=81000, bin_val=200, canny_min = 50, canny_max = 150, kernel_x = 3, kernel_y = 3)
+detector = Detector.Detector(color = [(13, 255, 152), (0, 51, 110)], light_min_area=5, kernel_x = 3, kernel_y = 3)
 tracker = Tracker.Tracker(frame_add = 1)
 serial = Serial.Serial(port='/dev/ttyS1', baudrate=115200, timeout=1, write_timeout=1)
 main()

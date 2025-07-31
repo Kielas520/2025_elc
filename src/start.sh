83a7514e86dc364efe1d5b34088d314622a4ee35
Kielas <c1470759@outlook.com>
2025-08-01 01:58:51 +0800

    efsfes

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "520" | sudo -S chmod 777 /home/kielas/2025_elc/src/start.sh
-echo "520" | sudo -S chmod 777 /home/kielas/2025_elc/src/main.py
-v4l2-ctl --list-devices  # 列出所有视频设备
+echo "520" | sudo -S chmod 777 /home/sunrise/2025_elc/src/start.sh
+echo "520" | sudo -S chmod 777 /home/sunrise/2025_elc/src/main.py
+/usr/bin/python3 /home/sunrise/2025_elc/src/main.py
+
+# v4l2-ctl --list-devices  # 列出所有视频设备
 # sudo rdk-miniboot-update
-/usr/bin/python3 /home/kielas/2025_elc/src/main.py
